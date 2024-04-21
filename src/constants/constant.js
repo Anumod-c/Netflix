f52cc1e5bb9b9f66b2b70e901e14b3e775360db3
Anumod-c <anumodc2001@gmail.com>
2024-04-21 14:42:31 +0530

    final commit

---
+++ src/constants/constant.js
@@ -0,0 +1,5 @@
+export const baseUrl="https://api.themoviedb.org/3";
+
+
+export const API_KEY="19e46993b446e06a938bd6d2b8ed31af";
+export const imageUrl="https://image.tmdb.org/t/p/original";
---
+++ src/url.js
@@ -0,0 +1,15 @@
+import { API_KEY } from "./constants/constant";
+
+
+export const orginals=`discover/tv?api_key=${API_KEY}&with_networks=213`;
+
+
+export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
+
+export const horror=`discover/movie?api_key=${API_KEY}&with_genres=27`
+
+
+export const comdedy=`discover/movie?api_key=${API_KEY}&with_genres=35`;
+export const romantic= `discover/movie?api_key=${API_KEY}&with_genres=10749`;
+
+export const documentries=`discover/movie?api_key=${API_KEY}&with_genres=99`
